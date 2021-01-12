22724d9026f475e715d0318446a7c2df6809a7e0
Potipecan <podpecan.andraz28@gmail.com>
2021-01-12 11:45:13 +0100

    added base table class, updated class Kraj

--- Database/Kraj.cs
+++ Database/Kraj.cs
@@ -6,17 +6,34 @@ using System.Threading.Tasks;
 
 namespace Database
 {
-    public class Kraj
+    public abstract class Table
     {
-        public string Name;
-        public string PostID;
-        public string Abbr;
+        public int ID { get; set; }
 
-        public Kraj(string name, string postid, string abbr = null)
+        protected Dictionary<string, string> columns;
+
+        public Table() {
+            columns = new Dictionary<string, string>();
+            columns.Add(nameof(ID), "id");
+        }
+    }
+    public class Kraj : Table
+    {
+        public string Name { get; set; }
+        public string PostID { get; set; }
+        public string Abbr { get; set; }
+
+        public Kraj(string name, string postid, string abbr = null) : base()
         {
+
             Name = name;
             PostID = postid;
             Abbr = abbr;
+
+            columns.Add(nameof(Name), "ime");
+            columns.Add(nameof(PostID), "posta");
+            columns.Add(nameof(Abbr), "kratica");
+            
         }
 
         public override string ToString()
@@ -24,4 +41,6 @@ namespace Database
             return $"{Name};{PostID};{Abbr}";
         }
     }
+
+    //public class /*User*/
 }
