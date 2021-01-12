14f871f899c7a29befa21d00adfc993bd5f4997f
Potipecan <podpecan.andraz28@gmail.com>
2021-01-12 12:09:55 +0100

    Added user class

--- Database/Kraj.cs
+++ Database/Kraj.cs
@@ -12,7 +12,8 @@ namespace Database
 
         protected Dictionary<string, string> columns;
 
-        public Table() {
+        public Table(int id = -1) {
+            ID = id;
             columns = new Dictionary<string, string>();
             columns.Add(nameof(ID), "id");
         }
@@ -44,9 +45,8 @@ namespace Database
         public string PostID { get; set; }
         public string Abbr { get; set; }
 
-        public Kraj(string name, string postid, string abbr = null) : base()
+        public Kraj(string name, string postid, string abbr = null, int id = -1) : base(id)
         {
-
             Name = name;
             PostID = postid;
             Abbr = abbr;
@@ -58,5 +58,50 @@ namespace Database
         }
     }
 
-    //public class /*User*/
+    public class User : Table
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public int Reg_ID { get; set; }
+        public int Projects_Num { get; set; }
+        public int Completed_Num { get; set; }
+        public string Password { get; set; }
+
+        public User(
+            string name,
+            string surname,
+            string username,
+            string email,
+            string address,
+            int reg_id,
+            int proj_num,
+            int comp_num,
+            string pass,
+            int id = -1
+            ) : base(id)
+        {
+            Name = name;
+            Surname = surname;
+            Username = username;
+            Email = email;
+            Address = address;
+            Reg_ID = reg_id;
+            Projects_Num = proj_num;
+            Completed_Num = comp_num;
+            Password = pass;
+
+            columns.Add(nameof(Name), "ime");
+            columns.Add(nameof(Surname), "priimek");
+            columns.Add(nameof(Email), "email");
+            columns.Add(nameof(Username), "uname");
+            columns.Add(nameof(Address), "naslov");
+            columns.Add(nameof(Reg_ID), "kraj_id");
+            columns.Add(nameof(Projects_Num), "st_projektov");
+            columns.Add(nameof(Completed_Num), "st_opravljenih_projektov");
+            columns.Add(nameof(Password), "geslo");
+        }
+    }
 }
